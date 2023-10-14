5ea4ad35efdd8ff7f32d5b63083b59b4271e4df9
VitalyKonces <140142399+VitalyKonces@users.noreply.github.com>
2023-10-15 01:34:28 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,16 @@
+}
+function main() {
+    data.forEach(item => {
+    return data;
+    });
+}
+    const data = [];
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+    const data = generateRandomData();
+function generateRandomData() {
+    }
+
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
