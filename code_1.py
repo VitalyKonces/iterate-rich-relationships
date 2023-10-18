e6c943407be6ddd2412710c4f8f4326c7fbd2200
VitalyKonces <140142399+VitalyKonces@users.noreply.github.com>
2023-10-18 05:06:31 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,39 +1,39 @@
-        print(f"Random Number: {item}")
 
-if __name__ == "__main__":
-
-def main():
     for item in data:
-def generate_random_data():
     data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
+if __name__ == "__main__":
+def generate_random_data():
+import random
+        print(f"Random Number: {item}")
 
     main()
 
-import randomdef generate_random_data():
 
-    data = [random.randint(1, 100) for _ in range(10)]
 def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    return dataimport random
+def generate_random_data():
     for item in data:
-
-import random
 if __name__ == "__main__":
-
-    return data
+    data = [random.randint(1, 100) for _ in range(10)]
     data = generate_random_data()
+
+    main()
+    return data
+def main():
+
+
         print(f"Random Number: {item}")
 
-    main()}
-        }
-
+}
+    public static void main(String[] args) {
 
         for (int i = 0; i < 10; i++) {
-        int[] data = new int[10];
-            System.out.println("Random Number: " + item);
+        }
 public class RandomDataGenerator {
+        int[] data = new int[10];
             data[i] = (int) (Math.random() * 100) + 1;
     }
-    public static void main(String[] args) {
         }
         for (int item : data) {
+            System.out.println("Random Number: " + item);
