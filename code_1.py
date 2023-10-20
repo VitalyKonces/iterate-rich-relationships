b1db01a226d0f590b273747083c668ec3d91d8e7
VitalyKonces <140142399+VitalyKonces@users.noreply.github.com>
2023-10-20 07:27:44 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,39 +1,51 @@
-
     for item in data:
+    main()
+def generate_random_data():
     data = generate_random_data()
 if __name__ == "__main__":
-def generate_random_data():
+
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+
+        print(f"Random Number: {item}")
 import random
-        print(f"Random Number: {item}")
+    return datadef generate_random_data():
+    for item in data:
 
-    main()
+if __name__ == "__main__":
 
 
 def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-    return dataimport random
-def generate_random_data():
-    for item in data:
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
+import random
+    return data
     data = generate_random_data()
 
+    data = [random.randint(1, 100) for _ in range(10)]
     main()
-    return data
-def main():
+        print(f"Random Number: {item}")        }
 
+        int[] data = new int[10];
 
-        print(f"Random Number: {item}")
-
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+        }
+    }
 }
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {public class RandomDataGenerator {
+
+        }
     public static void main(String[] args) {
 
-        for (int i = 0; i < 10; i++) {
-        }
-public class RandomDataGenerator {
-        int[] data = new int[10];
             data[i] = (int) (Math.random() * 100) + 1;
+}
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
     }
         }
-        for (int item : data) {
-            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
